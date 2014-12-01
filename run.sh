3604ad54877b94410750398397f54212756ce964
chawlasushant <sushant@jadoos-inc.com>
2014-12-01 11:56:28 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Start MySQL service
 /bin/sh /tmp/mysql.sh start
+
+# Set mysql root password
 mysqladmin -uroot password "test1234"
-/etc/init.d/httpd start
-tail -f /var/log/mysqld.log
+
+# Start apache in foreground
+/usr/sbin/httpd -DFOREGROUND
+
